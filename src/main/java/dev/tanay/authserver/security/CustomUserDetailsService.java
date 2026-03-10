167dbcabaaaf7b98463ca0f66577183e2bfb7c48
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-03-10 13:37:45 +0530

    Custom User details service added

--- src/main/java/dev/tanay/authserver/security/CustomUserDetailsService.java
+++ src/main/java/dev/tanay/authserver/security/CustomUserDetailsService.java
@@ -1,12 +1,23 @@
 package dev.tanay.authserver.security;
 
+import dev.tanay.authserver.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.Optional;
+
 public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        if(user.isEmpty()) throw new UsernameNotFoundException("User not found");
 
+        return new CustomUserDetails(user);
     }
 }
